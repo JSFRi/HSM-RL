df2a5f3b30288711f5bb7bf69095576a8c5fac3f
JSFRi <tianru.zhang@it.uu.se>
2023-05-15 17:25:20 +0200

    Update cloud deployment

--- cloud_deployment/app.py
+++ cloud_deployment/app.py
@@ -24,44 +24,16 @@ import matplotlib.pyplot as plt
 import io
 
 #Data imports
-Tier1 = pd.read_csv("/home/ubuntu/HSM-RL/production_server/tier1.csv")
-Tier2 = pd.read_csv("/home/ubuntu/HSM-RL/production_server/tier2.csv")
-Tier3 = pd.read_csv("/home/ubuntu/HSM-RL/production_server/tier3.csv")
+Tier1 = pd.read_csv("/home/ubuntu/HSM-RL/cloud_deployment/tier1.csv")
+Tier2 = pd.read_csv("/home/ubuntu/HSM-RL/cloud_deployment/tier2.csv")
+Tier3 = pd.read_csv("/home/ubuntu/HSM-RL/cloud_deployment/tier3.csv")
 
 #app = Flask(__name__, template_folder='./templates',static_folder='./static')
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return '<h1>Welcome to the Machine Learning Course.</h1>'
-
-@app.route("/accuracy", methods=['POST', 'GET'])
-def accuracy():
-    if request.method == 'POST':
-        r = get_accuracy.delay()
-        a = r.get()
-        return '<h1>The accuracy is {}</h1>'.format(a)
-
-    return '''<form method="POST">
-    <input type="submit">
-    </form>'''
-
-@app.route("/predictions", methods=['POST', 'GET'])
-def predictions():
-    if request.method == 'POST':
-        results = get_predictions.delay()
-        predictions = results.get()
-
-        results = get_accuracy.delay()
-        accuracy = results.get()
-        
-        final_results = predictions
-
-        return render_template('result.html', accuracy=accuracy ,final_results=final_results) 
-                    
-    return '''<form method="POST">
-    <input type="submit">
-    </form>'''
+    return render_template('index.html')
 
     
 #Pandas Page
